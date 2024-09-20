3f9e976036c67f2016a912458fe0fc374e14fc05
Rosa <164552124+rosamrcl@users.noreply.github.com>
2024-09-20 16:50:15 -0300

    .

--- js/culinaria.js
+++ js/culinaria.js
@@ -2,27 +2,22 @@
 
 //dados = json
 function dadosTela(dados){
+    console.log(dados)
     const caixa_media = document.querySelector(".caixa-media");
     
     let receita = document.createElement("h2");
+    receita.innerHTML=dados.receita;
+    
+    let caixa_menor = document.createElement("div");
     let tipo = document.createElement("p");
     let ingredientes = document.createElement("p");
     let preparo = document.createElement("p");
-    receita.innerHTML=dados.receita;
+    
+    caixa_menor.setAttribute("class","caixa-menor");
     tipo.innerHTML=dados.tipo;
     ingredientes.innerHTML=dados.ingradientes;
     preparo.innerHTML=dados.modo_preparo;
     
-    // let caixa_menor = document.createElement("div");
-    // let icone = document.createElement("img")
-    // caixa_menor.setAttribute("class","caixa-menor");
-    // icone.setAttribute("class","icone");
-    // icone.setAttribute("src", `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`);
-
-    // let previsao=document.createElement("p")
-    // let umidade=document.createElement("p")
-    // previsao.innerHTML=dados.weather[0].description
-    // umidade.innerHTML="Umidade: " + dados.main.humidity +"%"
     
     
     
@@ -32,30 +27,26 @@ function dadosTela(dados){
     caixa_menor.appendChild(tipo)
     caixa_menor.appendChild(ingredientes)
     caixa_media.appendChild(preparo)
-    // caixa_media.appendChild(temperatura)
-    // caixa_media.appendChild(caixa_menor)
+   
 
-    // document.querySelector(".city").innerHTML="Tempo em "+dados.name
-    // document.querySelector(".temperatura").innerHTML= "Temperatura: " + Math.floor(dados.main.temp-273.15)+" ºC"
-    // document.querySelector(".previsao").innerHTML=dados.weather[0].description
-    // document.querySelector(".umid").innerHTML="Umidade: " + dados.main.humidity +"%"
-    // document.querySelector(".icone").src=`https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+   
+    
+}
+
+    
+async function buscarReceita(){
+    
+    const dados = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://api-receitas-pi.vercel.app/receitas/todas')}`)
+                        .then((response) => response.json())
+                        .then((data) => console.log(data.contents))
+    console.log(dados)
     
 }
 
 
-async function buscarCidade(cidade){
-    const dados =await fetch (`https://api-receitas-pi.vercel.app/receitas/todas`).then(response=>response.json())
-    dadosTela(dados)
-     
-
-}
-
-
 function clicknoBotao(){
-    const cidade=document.querySelector(".cidade").value
-    buscarCidade(cidade)
+    const receita=document.querySelector(".receita").value
+    buscarReceita(receita)
 }
 
-buscarCidade('Maceió')
-// https://openweathermap.org/current#name
+buscarReceita()
